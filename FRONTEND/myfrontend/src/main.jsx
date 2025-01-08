9cedc990994c9af44032a10df6a3dcefbfbc38a2
j1ere <jeremywambua106@gmail.com>
2025-01-08 12:12:42 +0300

    token authentication success

--- FRONTEND/myfrontend/src/main.jsx
+++ FRONTEND/myfrontend/src/main.jsx
@@ -4,17 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import axios from "axios";
-import Cookies from "js-cookie";
+// import axios from "axios";
+// import Cookies from "js-cookie";
 import WordCount from "./components/WordCount"
 
-axios.interceptors.request.use((config) => {
-  const token = Cookies.get("jwtToken");
-  if (token) {
-    config.headers.Authorization = `Token ${token}`;
-  }
-  return config;
-});
+// axios.interceptors.request.use((config) => {
+//   const token = Cookies.get("jwtToken");
+//   if (token) {
+//     config.headers.Authorization = `Token ${token}`;
+//   }
+//   return config;
+// });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
